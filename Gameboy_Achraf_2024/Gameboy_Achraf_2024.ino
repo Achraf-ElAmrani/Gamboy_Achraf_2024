f784d5df7c81ebad30c025ec1e5d6765ef3251a7
Achraf <achrafelamranie@gmail.com>
2025-02-22 19:35:00 +0100

    commit 3
    suppressions des éléments inutiles et simplification de certaines lignes.

--- Gameboy_Achraf_2024/Gameboy_Achraf_2024.ino
+++ Gameboy_Achraf_2024/Gameboy_Achraf_2024.ino
@@ -1,40 +1,25 @@
 #include <Adafruit_ILI9341.h>  // Bibliothèque pour gérer l'écran ILI9341
 #include <Adafruit_GFX.h>      // Bibliothèque pour les fonctions graphiques
-//#include <I2S.h>
-/*
-const int frequency = 440; // frequency of square wave in Hz
-const int amplitude = 500; // amplitude of square wave
-const int sampleRate = 8000; // sample rate in Hz
 
-const int halfWavelength = (sampleRate / frequency); // half wavelength of square wave
-
-short sample = amplitude; // current sample value
-int count = 0;*/
-//int sample = 0;
 #define TFT_CS    10  // Broche de sélection de l'écran
 #define TFT_DC    12  // Broche de commande de données
 #define TFT_RST    6  // Broche de réinitialisation
-
-// Initialisation de l'écran ILI9341
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
-
 #define x A0  // Axe X du joystick
 #define y A2  // Axe Y du joystick
 
+// Initialisation de l'écran ILI9341
+Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
+
 // Position initiale du point
 int pointX = 120, pointY = 160;
-const int pointRadius = 5, threshold = 50, speed = 15; // Si la variation est inférieure à threshold = zone morte (50), elle est ignorée
+const int Radius = 5, threshold = 50, speed = 15; // Si la variation est inférieure à threshold = zone morte (50), elle est ignorée
 
 // Fonction pour dessiner un point à une position donnée avec une couleur spécifique
 void drawPoint(int x, int y, uint16_t color) {
-  tft.fillCircle(x, y, pointRadius, color);
+  tft.fillCircle(x, y, Radius, color);
 }
 
 void setup() {
-    /*if (!I2S.begin(I2S_PHILIPS_MODE, sampleRate, 16)) {
-    Serial.println("Failed to initialize I2S!");
-    while (1); // do nothing
-  }*/
   tft.begin();  // Initialisation de l'écran
   tft.setRotation(3);  // Orientation de l'écran
   tft.fillScreen(ILI9341_BLACK);  // Effacement de l'écran
@@ -46,21 +31,12 @@ void loop() {
   int oldX = pointX, oldY = pointY;  // Sauvegarde de l'ancienne position
 
   // Vérification des mouvements du joystick et mise à jour de la position
-  if (abs(xValue - 512) > threshold)
-  {
-     pointX -= map(xValue, 0, 1023, -speed, speed); // blague interpolation line
-      // I2S.write(sample);
-  }
-  if (abs(yValue - 512) > threshold)
-  {
-     pointY -= map(yValue, 0, 1023, -speed, speed); // Inversion ici
-       //I2S.write(sample);
-  }
-
+  if (abs(xValue - 512) > threshold) pointX -= map(xValue, 0, 1023, -speed, speed);
+  if (abs(yValue - 512) > threshold) pointY -= map(yValue, 0, 1023, -speed, speed);
 
   // Contraindre la position du point à l'intérieur des limites de l'écran
-  pointX = constrain(pointX, pointRadius, tft.width() - pointRadius);
-  pointY = constrain(pointY, pointRadius, tft.height() - pointRadius);
+  pointX = constrain(pointX, Radius, tft.width() - Radius);
+  pointY = constrain(pointY, Radius, tft.height() - Radius);
 
   drawPoint(oldX, oldY, ILI9341_BLACK);  // Effacer l'ancien point
   drawPoint(pointX, pointY, ILI9341_WHITE);  // Dessiner le nouveau point
