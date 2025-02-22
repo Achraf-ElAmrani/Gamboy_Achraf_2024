be9ff1971e4e05def543f81a38c8f52f03f37786
Achraf <achrafelamranie@gmail.com>
2025-02-22 20:04:53 +0100

    commit 4
    Ajout de la libraire MCP008 et define des des BP

--- Gameboy_Achraf_2024/Gameboy_Achraf_2024.ino
+++ Gameboy_Achraf_2024/Gameboy_Achraf_2024.ino
@@ -1,11 +1,19 @@
 #include <Adafruit_ILI9341.h>  // Bibliothèque pour gérer l'écran ILI9341
 #include <Adafruit_GFX.h>      // Bibliothèque pour les fonctions graphiques
+#include <Wire.h>
+#include <Adafruit_MCP23008.h> // Bibliothèque pour gérer le MCP23008
 
 #define TFT_CS    10  // Broche de sélection de l'écran
 #define TFT_DC    12  // Broche de commande de données
 #define TFT_RST    6  // Broche de réinitialisation
+// Définition des axe
 #define x A0  // Axe X du joystick
 #define y A2  // Axe Y du joystick
+// Définition des boutons connectés au MCP23008
+#define BUTTON1 0
+#define BUTTON2 1
+#define BUTTON3 2
+#define BUTTON4 3
 
 // Initialisation de l'écran ILI9341
 Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
