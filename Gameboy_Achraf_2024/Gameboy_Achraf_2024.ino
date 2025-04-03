193eac3b4dc1281f4f69d066fabd20e3ff3ac397
Achraf <achrafelamranie@gmail.com>
2025-04-03 08:28:24 +0200

    retour au comit
    retour au comit d'avant

--- Gameboy_Achraf_2024/Gameboy_Achraf_2024.ino
+++ Gameboy_Achraf_2024/Gameboy_Achraf_2024.ino
@@ -1,45 +1,42 @@
-// Programation console
-// Ahraf El Amrani
-// 6TQA-INRACI
-// Feather M0 Express - 8-bit Expander, Ecran ILI9341 - Joystick - Module son
-// 08.03.2025
-
-#include "console.h"
-int cpt = 0;
-
-void setup() {
-  Serial.begin(9600);
-  InitialisationEcran();
-  InitialisationButton();
-  Menu();
-  
-  while (Mode == 0) {
-      if (mcp.digitalRead(BUTTON1) == LOW) Mode = MODE_POINT;
-      if (mcp.digitalRead(BUTTON2) == LOW) Mode = MODE_INVADERS;
-  }
-    
-  tft.fillScreen(ILI9341_BLACK);
-
-  Time_ms = millis();
-
-  if (Mode == MODE_POINT) {
-      tft.setCursor(50, 100);
-      tft.println("Mode: Point");
-  } else if (Mode == MODE_INVADERS) {
-      tft.setCursor(50, 100);
-      tft.println("Mode: Invaders");
-  }
-}
-
-void loop() {
- if (Mode == MODE_POINT) {
-     if (millis() >= Time_ms + 1000) {
-      pointX = tft.width() / 2;
-      pointY = tft.height() / 2;
+ // Programation console
+ // Ahraf El Amrani
+ // 6TQA-INRACI
+ // Feather M0 Express - 8-bit Expander, Ecran ILI9341 - Joystick - Module son
+ // 08.03.2025
+ 
+ #include "console.h"
+ int cpt = 0;
+ 
+ void setup() {
+ 
+   Serial.begin(9600);
+   InitialisationEcran();
+   InitialisationButton();
+   Menu();
+ 
+   while (Mode == 0) {
+       if (mcp.digitalRead(BUTTON1) == LOW) Mode = MODE_POINT;
+       if (mcp.digitalRead(BUTTON2) == LOW) Mode = MODE_INVADERS;
+   }
+   //Mode = MODE_POINT;
+ 
+   tft.fillScreen(ILI9341_BLACK);
+   Time_ms = millis();
+   if (Mode == MODE_POINT) {
+       tft.setCursor(50, 100);
+   } else if (Mode == MODE_INVADERS) {
+       tft.setCursor(50, 100);
+       tft.println("Mode: Invaders");
+   }
+ }
+ 
+ void loop() {
+   if (Mode == MODE_POINT) {
+    /*if (millis() >= Time_ms + 1000) {
+      Time_ms = millis();
+     }*/
+       MettreAJourPosition();
+ }else if (Mode == MODE_INVADERS) {
+         //mode Space Invaders
      }
-      MettreAJourPosition();
-}else if (Mode == MODE_INVADERS) {
-        //mode Space Invaders
-    }
-
-}
+ }
--- Gameboy_Achraf_2024/console.h
+++ Gameboy_Achraf_2024/console.h
@@ -1,37 +1,37 @@
-#include <Adafruit_ILI9341.h>  // Bibliothèque pour gérer l'écran ILI9341
-#include <Adafruit_GFX.h>      // Bibliothèque pour les fonctions graphiques
-#include <Wire.h>
-#include <Adafruit_MCP23008.h> // Bibliothèque pour gérer le MCP23008
-
-#define TFT_CS    10  // Broche de sélection de l'écran
-#define TFT_DC    12  // Broche de commande de données
-#define TFT_RST    6  // Broche de réinitialisation
-// Définition des axe
-#define x A0  // Axe X du joystick
-#define y A2  // Axe Y du joystick
-// Définition des boutons connectés au MCP23008
-#define BUTTON1 0
-#define BUTTON2 1
-#define BUTTON3 2
-#define BUTTON4 3
-
-extern Adafruit_MCP23008 mcp;
-// Position initiale du point
-extern int pointX, pointY;
-// Si la variation est inférieure à threshold = zone morte (50), elle est ignorée
-extern const int Radius, threshold, speed; 
-// Initialisation de l'écran ILI9341
-extern Adafruit_ILI9341 tft;
-
-extern byte Mode;
-extern byte MODE_POINT;
-extern byte MODE_INVADERS;
-extern unsigned long Time_ms;
-
-void drawPoint(int, int, uint16_t);
-void Menu(void);
-void InitialisationButton(void);
-void InitialisationEcran(void);
-void MettreAJourPosition(void);
-bool buttonappuyer(int button);
-int Deplacement(int ValeurJoystick);
+ #include <Adafruit_ILI9341.h>  // Bibliothèque pour gérer l'écran ILI9341
+ #include <Adafruit_GFX.h>      // Bibliothèque pour les fonctions graphiques
+ #include <Wire.h>
+ #include <Adafruit_MCP23008.h> // Bibliothèque pour gérer le MCP23008
+ 
+ #define TFT_CS    10  // Broche de sélection de l'écran
+ #define TFT_DC    12  // Broche de commande de données
+ #define TFT_RST    6  // Broche de réinitialisation
+ // Définition des axe
+ #define x A0  // Axe X du joystick
+ #define y A2  // Axe Y du joystick
+ // Définition des boutons connectés au MCP23008
+ #define BUTTON1 0
+ #define BUTTON2 1
+ #define BUTTON3 2
+ #define BUTTON4 3
+ 
+ extern Adafruit_MCP23008 mcp;
+ // Position initiale du point
+ extern int pointX, pointY;
+ // Si la variation est inférieure à threshold = zone morte (50), elle est ignorée
+ extern const int Radius, threshold, speed; 
+ // Initialisation de l'écran ILI9341
+ extern Adafruit_ILI9341 tft;
+ 
+ extern byte Mode;
+ extern byte MODE_POINT;
+ extern byte MODE_INVADERS;
+ extern unsigned long Time_ms;
+ 
+ void drawPoint(int, int, uint16_t);
+ void Menu(void);
+ void InitialisationButton(void);
+ void InitialisationEcran(void);
+ void MettreAJourPosition(void);
+ bool buttonappuyer(int button);
+ int Deplacement(int ValeurJoystick);
